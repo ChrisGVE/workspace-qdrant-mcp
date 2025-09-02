73051377b3ceb45e829dd3b80af271a8992a7c65
Christian C. Berclaz <christian.berclaz@mac.com>
2025-09-02 23:16:09 +0200

    feat: Enable protobuf code generation in build.rs
    - Activate tonic_build to generate Rust code from proto files
    - Configure both server and client code generation
    - Add serde traits for serialization support
    - Set up rebuild trigger when proto files change

--- rust-engine/grpc/build.rs
+++ rust-engine/grpc/build.rs
@@ -1,13 +1,15 @@
 // Build script for gRPC proto generation
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // For now, just a placeholder build script
-    // In the future, this will generate Rust code from proto files
+    // Generate Rust code from protobuf definitions
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(true)
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .compile(&["../proto/ingestion.proto"], &["../proto"])?;
 
-    // tonic_build::configure()
-    //     .build_server(true)
-    //     .build_client(true)
-    //     .compile(&["../proto/ingestion.proto"], &["../proto"])?;
+    // Rerun if proto files change
+    println!("cargo:rerun-if-changed=../proto/ingestion.proto");
 
     Ok(())
 }
